f6e59fb2b1fe964b86039fdc4dc52c142a3d0e3e
HAKUHO <hakuho01@gmail.com>
2023-07-30 13:30:20 +0900

    3凸後報告要求機能を実装

--- bot.rb
+++ bot.rb
@@ -15,4 +15,8 @@ bot.message(contains: %r{https://twitter.com/([a-zA-Z0-9_]+)/status/([0-9]+)}) d
   bot_controller.handle_message(event, :thumb)
 end
 
+bot.message(contains: '<:0cb_3totsume:986725406105280582>') do |event|
+  bot.send_message(channel_id=979757567200858183, "<@!#{event.user.id}>3凸目が終了したら https://discord.com/channels/612729411271131141/1112447156197007360 と https://discord.com/channels/612729411271131141/950253786897743883 で凸報告をお願いします:pray:\n今日もクラバトお疲れ様でした<:NGOD_saikouka:936573410836877372>")
+end
+
 bot.run
--- controller/bot_controller.rb
+++ controller/bot_controller.rb
@@ -15,7 +15,9 @@ class BotController < Component
   def handle_message(event, message_type)
     case message_type
     when :thumb
-      @api_service.twitter_thumbnail(event)
+      @api_service.twitter_thumbnail(bot,event)
+    when :santotsu
+      bot.send_message(channel_id=725471441260118097, "返す文字列")
     end
   end
 end
