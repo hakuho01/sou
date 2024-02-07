902976f7653f19289aebf0be16ba0ba070c6d4b5
HAKUHO <hakuho01@gmail.com>
2024-02-07 21:33:04 +0900

    bugfix

--- service/api_service.rb
+++ service/api_service.rb
@@ -12,7 +12,7 @@ class ApiService < Component
   # TwitterNSFWサムネイル表示
   def twitter_thumbnail(event)
     # discordが展開しているか確認する
-    sleep ２
+    sleep 2
     event_msg_id = event.message.id.to_s
     event_msg_ch = event.message.channel.id.to_s
 
