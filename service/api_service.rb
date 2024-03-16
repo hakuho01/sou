ae55427d9f5c19adb2ece876347117677c4ccd4a
HAKUHO <hakuho01@gmail.com>
2024-03-16 18:02:49 +0900

    discordの不具合で展開されなかったツイートを展開するように

--- service/api_service.rb
+++ service/api_service.rb
@@ -19,7 +19,7 @@ class ApiService < Component
     uri = URI.parse("https://discord.com/api/channels/#{event_msg_ch}/messages/#{event_msg_id}")
     res = Net::HTTP.get_response(uri, 'Authorization' => "Bot #{TOKEN}")
     parsed_res = JSON.parse(res.body)
-    return unless parsed_res['embeds'].nil? || parsed_res['embeds'][0]['title'] == 'X' # discordが埋め込みやってなければ以下へ進む
+    return unless parsed_res['embeds'].empty? || parsed_res['embeds'][0]['title'] == 'X' # discordが埋め込みやってなければ以下へ進む
 
     # ツイート情報を取得する
     content = event.message.content
